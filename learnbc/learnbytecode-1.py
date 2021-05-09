8b44fb7e03a42ace43659b492b99fe60b264d1e5
spxai <lx@myhaspl.com>
2021-05-09 18:10:32 +0800

    ud

--- learnbc/learnbytecode-1.py
+++ learnbc/learnbytecode-1.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #learnbytecode-1.py
-print("hello,world!")
+import dis
+import platform
+
+def sayHello():
+    print("hello,world!")
+print("python Version:",end="")
+print(platform.python_version())
+print("---------------------")
+print(dis.dis(sayHello))
