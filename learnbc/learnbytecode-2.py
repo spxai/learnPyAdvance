e5b87da435b12848f49707045251ae6cedf273b4
spxai <lx@myhaspl.com>
2021-05-12 18:55:03 +0800

    Update learnbytecode-2.py

--- learnbc/learnbytecode-2.py
+++ learnbc/learnbytecode-2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #learnbytecode-2.py
-
 import platform
 
 def sayHello():
@@ -9,8 +8,10 @@ def sayHello():
 print("python Version:",end="")
 print(platform.python_version())
 print("---------------------")
-codeAttributes=list(dir(sayHello.__code__))
+codeAttributes=dir(sayHello.__code__)
 for codeAttr in codeAttributes:
-    if "co_" in codeAttr:
+    if "co_" in codeAttr:     
         print(codeAttr,end=":")
-        print(codeAttr)
+        print(getattr(sayHello.__code__,codeAttr))
+
+        
