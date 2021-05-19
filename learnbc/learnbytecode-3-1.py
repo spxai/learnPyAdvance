db3afa97b168e0e463db21f4c4e656943a1aa330
spxai <lx@myhaspl.com>
2021-05-19 17:51:00 +0800

    ud

--- learnbc/learnbytecode-3-1.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#learnbytecode-3-1.py
-x="hello,world"
-print(x)
--- learnbc/learnbytecode-3-3.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#learnbytecode-3-3.py
-import marshal, dis
-
-f = open("__pycache__/learnbytecode-3-1.cpython-39.pyc ", "rb")
-f.seek(16) # Skip 16 byte header (for Python 3.8)
-co = marshal.load(f)
-print(dis.dis(co))
