dd006a71a4749a68442c008a6295852994e4cf16
spxai <lx@myhaspl.com>
2021-05-19 21:26:01 +0800

    ud

--- learnbc/learnbytecode-3-1.py
+++ learnbc/learnbytecode-3-1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #learnbytecode-3-1.py
-x="hello,world"
+x="hello,world.\nlearn python"
 print(x)
--- learnbc/learnbytecode-3-5.py
+++ learnbc/learnbytecode-3-5.py
@@ -8,38 +8,43 @@ import codecs
 import importlib.util
 import sys
 
+def getFileInfo(fname):
+    import os
+    statinfo = os.stat(fname)
+    return (statinfo.st_size,int(statinfo.st_mtime))
+
 
 print (f"python version:{sys.version}")
-
-
-
-
 f = open("__pycache__/learnbytecode-3-1.cpython-39.pyc ", "rb")
 magic = f.read(4)
 bitField=f.read(4)
 moddate = f.read(4)
 fileSize=f.read(4)
+fileSz=struct.unpack("=L", fileSize)[0]
 code=f.read()
-modtime = time.strftime("%Y-%m-%d %H:%M:%S",(time.localtime(struct.unpack('L', moddate)[0])))
+modtime = time.strftime("%Y-%m-%d %H:%M:%S",(time.localtime(struct.unpack('=L', moddate)[0])))
 print(f"magic {codecs.encode(magic,'hex')}")
 print(f"bitField {codecs.encode(bitField,'hex')}")
 print(f"moddate :{modtime}")
-print(f"fileSize :{codecs.encode(fileSize,'hex')}")
+print(f"fileSize :{fileSz}")
 f.seek(16)
 co = marshal.load(f)
 print(dis.dis(co))
 f.close()
 
+sFileInfo=getFileInfo("learnbytecode-3-1.py")
 magic_generate=importlib.util.MAGIC_NUMBER
 bitField_generate=(0).to_bytes(4, byteorder="big")
-moddate_generate=struct.pack('L',int(time.time()))
+
+moddate_generate=struct.pack('=L',sFileInfo[1])
+srcFileSize_generate=struct.pack('=L',sFileInfo[0])
+
 f = open("byc-generate/learnbytecode-3-1.cpython-39.pyc","wb")
 f.write(magic_generate)
 f.write(bitField_generate)
 f.write(moddate_generate)
-f.write(fileSize)
+f.write(srcFileSize_generate)
 f.write(code)
-f.f
 f.close()
 
 f = open("byc-generate/learnbytecode-3-1.cpython-39.pyc ", "rb")
@@ -47,12 +52,13 @@ magic = f.read(4)
 bitField=f.read(4)
 moddate = f.read(4)
 fileSize=f.read(4)
+fileSz=struct.unpack("=L", fileSize)[0]
 code=f.read()
-modtime = time.strftime("%Y-%m-%d %H:%M:%S",(time.localtime(struct.unpack('L', moddate)[0])))
+modtime = time.strftime("%Y-%m-%d %H:%M:%S",(time.localtime(struct.unpack('=L', moddate)[0])))
 print(f"magic {codecs.encode(magic,'hex')}")
 print(f"bitField {codecs.encode(bitField,'hex')}")
 print(f"moddate :{modtime}")
-print(f"fileSize :{codecs.encode(fileSize,'hex')}")
+print(f"fileSize :{fileSz}")
 f.seek(16)
 co = marshal.load(f)
 print(dis.dis(co))
