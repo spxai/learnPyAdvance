cdcbb162df8308963aa6c8c8695ddf777af40d4d
spxai <lx@myhaspl.com>
2021-05-19 17:51:33 +0800

    ct

---
+++ learnbc/learnbytecode-3-1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#learnbytecode-3-1.py
+x="hello,world"
+print(x)
---
+++ learnbc/learnbytecode-3-2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#learnbytecode-3-2.py
+import py_compile
+py_compile.compile('learnbytecode-3-1.py')
